d8f925f2df5dcea2c0f78e04a3fea696210e1c67
Jonathan Dai <nightghost5078@gmail.com>
2016-01-10 14:00:26 +0800

    Fix style.

--- ES6/retry.js
+++ ES6/retry.js
@@ -2,18 +2,18 @@ import 'babel-polyfill';
 
 function enter(str1, str2) {
   return new Promise((resolve, reject) => {
-      let random = Math.random();
-  if (random > 0.5) {
-    reject(new Error('failed, try again'));
-  } else {
-    resolve(str1 + str2 + ' : ' + random);
-  }
-});
+    let random = Math.random();
+    if (random > 0.5) {
+      reject(new Error('failed, try again'));
+    } else {
+      resolve(str1 + str2 + ' : ' + random);
+    }
+  });
 }
 
 function retry(fn, args, retryLeft = 3) {
   console.log('args: ', args);
-  return fn.apply(null, args).catch(function(err) {
+  return fn.apply(null, args).catch((err) => {
     console.log(fn.name + ' failed: ', err, 'retry left: ', retryLeft);
     if (retryLeft <= 0) {
       console.log('max retry approach');
@@ -28,7 +28,7 @@ function enterRetry() {
     retry(enter, ['name is ', 'jonathan']).then((_) => {
       console.log('final', _);
       resolve();
-    }, (err) => {
+    }).catch((err) => {
       console.log('wrapped err: ', err);
       resolve();
     });
